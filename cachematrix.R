d97675477bd004c4e4eefbaeb3c5cceb84fc99ef
thomasevans <thomas.jude.evans@gmail.com>
2017-11-03 09:24:30 +0000

    Cache matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that first cache a copy of a matrix, and when
+## first executed also cache its inverse.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x  
+        setInv <- function(solve) inv <<- solve
+        getInv <- function() inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function interacts with our cahced matrix object.
+## If it is a new matrix a new matrix object is created and
+## its inverse calculated (and cached). If it is not a new
+## matrix object then the cached inverse is recalled.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInv(inv)
+        inv
+        
 }
+
+## Examples of running these functions
+# Repreducible seed for 'random' matrices.
+set.seed(1)
+
+# Make a first matrix
+mat1 <- makeCacheMatrix(matrix(sample(100,9),3))
+# Get inverse
+cacheSolve(mat1)
+# Get inverse a second time (now uses cached value)
+cacheSolve(mat1)
+
+
+# Make a second matrix
+mat1 <- makeCacheMatrix(matrix(sample(100,9),3))
+# Get inverse
+cacheSolve(mat1)
+# Get inverse a second time (now uses cached value)
+cacheSolve(mat1)
